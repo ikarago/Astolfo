caa3e9d08d94ab594f8c1ebb69c131842a01bb22
Ikarago <ikarago@outlook.com>
2019-03-11 20:40:57 +0100

    Add project files.

---
+++ Astolfo/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using Astolfo.Helpers;
+
+namespace Astolfo.ViewModels
+{
+    public class MainViewModel : Observable
+    {
+        public MainViewModel()
+        {
+        }
+    }
+}
